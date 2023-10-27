0969e24e8f3a5053ae9a0bb279f116d662c5c78e
Levente Major <leventemajor36@gmail.com>
2023-10-27 10:00:48 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Szoftverek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

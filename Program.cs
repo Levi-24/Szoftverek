70d1811002401316a3d4c7009619d329ea74f768
Levi-24 <leventemajor36@gmail.com>
2023-11-02 14:21:51 +0100

    Completed

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Linq;
 
 namespace Szoftverek
 {
@@ -6,7 +10,99 @@ namespace Szoftverek
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hazífeladat");
+            List<Szoftver> szoftverek = new();
+            using (StreamReader sr = new(@"..\..\..\src\szoftverek.txt", Encoding.UTF8))
+            {
+                _ = sr.ReadLine();
+                while (!sr.EndOfStream)
+                {
+                    szoftverek.Add(new Szoftver(sr.ReadLine()));
+                }
+            }
+
+            Console.WriteLine("7.Feladat:");
+            List<Szoftver> antivirusok = Feladat7(szoftverek);
+            Console.WriteLine($"\t{antivirusok.Count}");
+
+            Console.WriteLine("8.Feladat:");
+            List<Szoftver> majdnemLegjobb = new();
+            double legjobbErtekeles = szoftverek.Max(x => x.Ertekeles);
+            Feladat8(szoftverek, legjobbErtekeles, majdnemLegjobb);
+
+            foreach (var szoftver in majdnemLegjobb)
+            {
+                Console.WriteLine($"\t{szoftver}");
+            }
+            Console.WriteLine($"\t{majdnemLegjobb.Count}");
+            Console.WriteLine($"\t{legjobbErtekeles}");
+
+            Console.WriteLine("10.Feladat:");
+            Console.WriteLine($"\t{Feladat10(szoftverek)}");
+
+            Console.WriteLine("11.Feladat:");
+            List<string> kategoriak = Feladat11(szoftverek);
+            foreach (var kategoria in kategoriak)
+            {
+                Console.WriteLine($"\t{kategoria}");
+            }
+
+            Console.WriteLine("12.Feladat:");
+        }
+
+        static List<Szoftver> Feladat7(List<Szoftver> szoftverek)
+        {
+            List<Szoftver> megfelelo = new();
+            foreach (var szoftver in szoftverek)
+            {
+                if (szoftver.Ertekeles > 8.5 && szoftver.Kategoria == "antivírus")
+                {
+                    megfelelo.Add(szoftver);
+                }
+            }
+            return megfelelo;
+        }
+        static void Feladat8(List<Szoftver> szoftverek, double legjobbErtekeles, List<Szoftver> majdnemLegjobb)
+        {
+            majdnemLegjobb.AddRange(szoftverek.Where(x => x.Ertekeles == legjobbErtekeles - 0.1));
+        }
+        static double Feladat10(List<Szoftver> szoftverek)
+        {
+            int megfelel = szoftverek.Count(s => s.NevEsVerzioszam.StartsWith("Adobe"));
+            double megfelelOsszeg = 0;
+            foreach (var szoftver in szoftverek)
+            {
+                if (szoftver.NevEsVerzioszam.StartsWith("Adobe"))
+                {
+                    megfelelOsszeg += szoftver.BruttoAr();
+                }
+            }
+            double bruttoAtlag = megfelelOsszeg / megfelel;
+            return bruttoAtlag;
+        }
+        static List<string> Feladat11(List<Szoftver> szoftverek)
+        {
+            List<string> kategoriak = new();
+            foreach (var szoftver in szoftverek)
+            {
+                if (szoftver.OpRendszerek.Contains(',') && !kategoriak.Contains(szoftver.Kategoria))
+                {
+                    kategoriak.Add(szoftver.Kategoria);
+                }
+            }
+            kategoriak.Sort();
+            return kategoriak;
+        }   
+        static List<int> Feladat12(List<Szoftver> szoftverek)
+        {
+            List<int> azonositok = new();
+            foreach (var szoftver in szoftverek)
+            {
+                if (szoftver.NettoAr > 500 && szoftver.Ertekeles < 9)
+                {
+                    azonositok.Add(szoftver.Azonosito);
+                }
+            }
+            return azonositok;
         }
     }
 }
--- Szoftver.cs
+++ Szoftver.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,37 @@ namespace Szoftverek
 {
     class Szoftver
     {
+        //azonosító|név és verziószám|licenc típus|operációs rendszerek|kategória|felhasználói értékelés|nettó ár|adókulcs
+        public int Azonosito { get; set; }
+        public string NevEsVerzioszam { get; set; }
+        public string LicencTipus { get; set; }
+        public string OpRendszerek { get; set; }
+        public string Kategoria { get; set; }
+        public double Ertekeles { get; set; }
+        public double NettoAr { get; set; }
+        public int Adokulcs { get; set; }
+
+        public Szoftver(string sor)
+        {
+            string[] adatok = sor.Split('|');
+            Azonosito = Convert.ToInt32(adatok[0]);
+            NevEsVerzioszam = adatok[1];
+            LicencTipus = adatok[2];
+            OpRendszerek = adatok[3];
+            Kategoria = adatok[4];
+            Ertekeles = Convert.ToDouble(adatok[5]);
+            NettoAr = Convert.ToDouble(adatok[6]);
+            Adokulcs = Convert.ToInt32(adatok[7]);
+        }
+
+        public double BruttoAr()
+        {
+            return NettoAr + (NettoAr * (Adokulcs / 100));
+        }
+
+        public override string ToString()
+        {
+            return $"Azonosito: {Azonosito}, NevEsVerzioszam: {NevEsVerzioszam}, LicencTipus: {LicencTipus}, OpRendszerek: {OpRendszerek}, Kategoria: {Kategoria}, Ertekeles: {Ertekeles}, NettoAr: {NettoAr}, Adokulcs: {Adokulcs};";
+        }
     }
 }
